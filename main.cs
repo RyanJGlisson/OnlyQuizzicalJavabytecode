f8961cd9ce2e6f48a2436eb8353321f1f326d4e6
RyanJGlisson <143763150+RyanJGlisson@users.noreply.github.com>
2023-10-08 22:26:43 +0000

    Update

--- main.cs
+++ main.cs
@@ -2,6 +2,57 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World!!");
+   //unit 1
+    string name1 = "Lucinda Potter";
+    string DOB1 = "6/24/1992";
+    string work1 = "work 000-000-0101";
+    string cell1 = "cell 000-000-0189";
+  //unit 2
+    string name2 = "John Smith";
+    string DOB2 = "8/15/1978";
+    string work2 = "work 000-111-0101";
+    string cell2 = "cell 000-111-0189";
+  //unit 3
+    string name3 = "Chris Johnson";
+    string DOB3 = "12/01/1987";
+    string work3 = "work 000-222-0101";
+    string cell3 = "cell 000-222-0189";
+  //unit 4
+    string name4 = "Robert Hall";
+    string DOB4 = "2/27/1949";
+    string work4 = "work 000-444-0101";
+    string cell4 = "cell 000-444-0189";
+  //unit1 PRINT
+    Console.WriteLine ("Unit #1");
+    Console.WriteLine ();
+    Console.WriteLine (name1);
+    Console.WriteLine (DOB1);
+    Console.WriteLine (work1);
+    Console.WriteLine (cell1);
+    Console.WriteLine ();
+  //unit 2
+    Console.WriteLine ("Unit #2");
+    Console.WriteLine ();
+    Console.WriteLine (name2);
+    Console.WriteLine (DOB2);
+    Console.WriteLine (work2);
+    Console.WriteLine (cell2);
+    Console.WriteLine ();
+  //unit 3
+    Console.WriteLine ("Unit #3");
+    Console.WriteLine ();
+    Console.WriteLine (name3);
+    Console.WriteLine (DOB3);
+    Console.WriteLine (work3);
+    Console.WriteLine (cell3);
+    Console.WriteLine ();
+  //unit 4 
+    Console.WriteLine ("Unit #4");
+    Console.WriteLine ();
+    Console.WriteLine (name4);
+    Console.WriteLine (DOB4);
+    Console.WriteLine (work4);
+    Console.WriteLine (cell4);
+    
   }
 }
